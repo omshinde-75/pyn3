6e91dbf9bd3510f02205f117ccfcaf41e5461baa
omshinde-75 <om123shinde12345@gmail.com>
2026-08-18 01:10:01 +0530

    first commit

---
+++ test.py
@@ -0,0 +1,24 @@
+## single line comment
+''' multiline 
+
+comment'''
+
+## python is case sensitive 
+
+# name = "Om"
+# Name = "Fam"
+
+# print(name)
+# print(Name)
+
+
+# total = 1+2+3+4+\
+# 9+3+2
+# print(total)
+
+# x=5;y=10;z=x+y
+# # print(z)
+
+# age = 21
+# print(type(age))
+
